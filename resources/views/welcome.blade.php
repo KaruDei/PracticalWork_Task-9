1807039418b2ea9db4a44b6db50dfffc80c2d690
Просвернин Ярослав <1myoffice13@gmail.com>
2023-06-05 17:11:23 +0400

    Update welcome.blade.php file

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Task9-laravel</title>
-</head>
-<body>
-	
-</body>
-</html>
+@extends('layouts.structure')
+
+@section('title')Авторизация@endsection
+
+
+@section('content')
+
+	<div class="auth_container">
+		
+	</div>
+
+@endsection
