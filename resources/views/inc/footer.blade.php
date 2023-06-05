7c21f594f16c2a737fd074adb7c752b9b30336cc
Просвернин Ярослав <1myoffice13@gmail.com>
2023-06-05 17:05:02 +0400

    Add inc folder, add  header and footer files

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+	<p>Task 9 - Laravel</p>
+	<p>&copy; 2023</p>
+</footer>
---
+++ resources/views/inc/header.blade.php
@@ -0,0 +1,18 @@
+
+<header class="header">
+	<div class="container">
+		<div class="header_flex">
+			<div class="Logo">
+				<h1><a href="">Library</a></h1>
+			</div>
+
+			<nav class="menu">
+				<a href="">Books</a>
+				<a href="">People</a>
+				<a href="">Profile</a>
+				<a href="">Sing In</a>
+				<a href="">Sing Up</a>
+			</nav>
+		</div>
+	</div>
+</header>
