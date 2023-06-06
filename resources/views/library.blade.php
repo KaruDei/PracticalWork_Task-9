f5adc6237dff8133fd4576e0044c2fceaa882afd
Просвернин Ярослав <1myoffice13@gmail.com>
2023-06-06 18:48:11 +0400

    Add library file

---
+++ resources/views/library.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.structure')
+
+@section('title') Library @endsection
+
+
+@section('content')
+
+	<div class="book_items">
+		<a href="#" class="book_item">
+			<img src="{{ asset('imgs/library/2524023.jpg') }}" alt="">
+		</a>
+		
+		<a href="#" class="book_item">
+			<img src="{{ asset('imgs/library/2559539_detail.jpg') }}" alt="">
+		</a>
+		
+		<a href="#" class="book_item">
+			<img src="{{ asset('imgs/library/32127989_sluk.jpg') }}" alt="">
+		</a>
+		
+		<a href="#" class="book_item">
+			<img src="{{ asset('imgs/library/Снимок экрана 2023-06-06 151608.jpg') }}" alt="">
+		</a>
+
+		
+		<a href="#" class="book_item">
+			<img src="{{ asset('imgs/library/Снимок экрана 2023-06-06 151608.jpg') }}" alt="">
+		</a>
+
+		<a href="#" class="book_item">
+			<img src="{{ asset('imgs/library/32127989_sluk.jpg') }}" alt="">
+		</a>
+		
+		<a href="#" class="book_item">
+			<img src="{{ asset('imgs/library/2559539_detail.jpg') }}" alt="">
+		</a>
+
+		<a href="#" class="book_item">
+			<img src="{{ asset('imgs/library/2524023.jpg') }}" alt="">
+		</a>
+		
+	</div>
+
+@endsection
