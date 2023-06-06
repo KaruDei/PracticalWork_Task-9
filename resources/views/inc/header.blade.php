a07b3986028ee997aa1f901987dd7c666af23e42
Просвернин Ярослав <1myoffice13@gmail.com>
2023-06-06 18:51:15 +0400

    Update header file

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,17 +1,17 @@
-
 <header class="header">
 	<div class="container">
 		<div class="header_flex">
 			<div class="Logo">
-				<h1><a href="">Library</a></h1>
+				<h1><a href="">LIBRARY</a></h1>
 			</div>
 
 			<nav class="menu">
-				<a href="">Books</a>
-				<a href="">People</a>
-				<a href="">Profile</a>
-				<a href="">Sing In</a>
-				<a href="">Sing Up</a>
+				<a href="/">Books</a>
+				<a href="people">People</a>
+				<a href="profile">Profile</a>
+				<a href="auth">Sing In</a>
+				<a href="reg">Sing Up</a>
+				<a href="exit">Exit</a>
 			</nav>
 		</div>
 	</div>
