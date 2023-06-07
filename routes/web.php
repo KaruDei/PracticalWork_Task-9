59ddc08864699e90b1fc69d25c28bcb32f072e91
Просвернин Ярослав <1myoffice13@gmail.com>
2023-06-07 17:10:25 +0400

    Update web.php route file

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,20 +14,61 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 	return view('library');
-});
+}) -> name('home');
+
+Route::get('/exit', function () {
+	return view('library');
+}) -> name('exit');
+
+/* AUTHORIZATION */
 
 Route::get('/auth', function () {
 	return view('authorization');
-});
+}) -> name('auth');
+
+Route::post('/auth/submit', 'App\Http\Controllers\UsersController@auth') -> name('auth-form');
+
+/* REGISTRATION */
 
 Route::get('/reg', function () {
 	return view('registration');
-});
+}) -> name('reg');
+
+Route::post('/reg/submit', 'App\Http\Controllers\UsersController@reg') -> name('reg-form');
+
+/* PROFILE */
 
 Route::get('/profile', function () {
 	return view('profile');
-});
+}) -> name('profile');
 
-Route::get('/people', function () {
-	return view('people');
-});
+Route::post('/profile/comment', function () {
+	
+}) -> name('comment-form');
+
+Route::get('/profile/edit-profile', function () {
+	return view('profile');
+}) -> name('edit-profile');
+
+Route::get('/profile/my-comments', function () {
+	return view('profile');
+}) -> name('my-comments');
+
+/* PEOPLE */
+
+// Route::get('/people', function () {
+// 	return view('people');
+// }) -> name('people');
+
+Route::get('/people', 'App\Http\Controllers\UsersController@out_users') -> name('people');
+
+Route::get('/profile/id', function () {
+	return view('profile');
+}) -> name('people-profile');
+
+
+/* LIBRARY */
+
+Route::get('/book/id', function () {
+	return view('library');
+}) -> name('book-page');
