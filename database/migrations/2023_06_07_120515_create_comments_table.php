e60ee6f2e6d3045138b5bc7e72bbbbdad9845a31
Просвернин Ярослав <1myoffice13@gmail.com>
2023-06-08 21:19:41 +0400

    Update create_comments_table migrate

--- database/migrations/2023_06_07_120515_create_comments_table.php
+++ database/migrations/2023_06_07_120515_create_comments_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
 			  $table->id();
-			  $table->id('user_id');
+			  $table->integer('user_id');
 			  $table->string('title');
 			  $table->text('text');
 			  $table->timestamps();
