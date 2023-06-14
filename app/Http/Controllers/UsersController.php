201cf6f5f8dadf56470073c1f35cec820d63c4f5
Просвернин Ярослав <1myoffice13@gmail.com>
2023-06-14 17:07:09 +0400

    Update UsersController file

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\RegistrationRequest;
 use App\Http\Requests\AuthorizationRequest;
 use App\Http\Requests\Edit_profileRequest;
+use App\Http\Requests\CommentRequest;
 use App\Models\Comments;
 use Illuminate\Http\Request;
 use App\Models\User;
@@ -53,7 +54,11 @@ class UsersController extends Controller
 	public function out_user($id) {
 		// $user = new Users;
 		// $user -> all();
-		return view('profile', ['data' => User::find($id), 'comments' => Comments::all()->where('profile_id', '==', $id)->take(5)]);
+		$data = User::find($id);
+		$comments = Comments::all()->where('profile_id', '==', $id)->sortByDesc('id')->take(5);
+		$usersid = Comments::all()->pluck('user_id');
+		$users = User::whereIn('id', $usersid)->get();
+		return view('profile', ['data' => $data, 'comments' => $comments, 'users' => $users]);
 	}
 
 	public function edit_profile($id) {
@@ -81,13 +86,32 @@ class UsersController extends Controller
 		return redirect() -> route('home') -> with('success', 'Deleted');
 	}
 
-	public function add_comment($id, ) {
-		
-		// return redirect() -> route('home');
+	public function add_comment(CommentRequest $req) {
+		$comment = new Comments();
+		$comment -> profile_id = $req -> input('profile_id');
+		$comment -> user_id = $req -> input('user_id');
+		$comment -> comment_id = $req -> input('comment_id');
+		if ($comment -> comment_id == '') {
+			$comment -> comment_id = NULL;
+		}
+		$comment -> title = $req -> input('comment_title');
+		$comment -> text = $req -> input('comment_text');
+
+		$comment -> save();
+		return redirect() -> back();
 	}
 
-	public function delete_comment() {
-		
+	public function delete_comment($id, $del) {
+		Comments::find($del) -> delete();
+		return redirect() -> back();
+	}
+
+	public function my_comments($id) {
+		$data = User::find($id);
+		$comments = Comments::all() -> where('user_id', '==', $id);
+		$usersid = Comments::all()->pluck('user_id');
+		$users = User::whereIn('id', $usersid)->get();
+		return view('my_comments', ['data' => $data, 'comments' => $comments, 'users' => $users]);
 	}
 
 }
