19999a7dab4ffc0535efe43d447f6611a3f1e5ec
Просвернин Ярослав <1myoffice13@gmail.com>
2023-06-14 17:08:18 +0400

    Failed attempt to work with ajax

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
 import './bootstrap';
+
+$(document).ready(function() {
+    $('#loadAllComments').click(function(e) {
+        e.preventDefault(); // Предотвращаем переход по ссылке по умолчанию
+        
+        // Выполняем AJAX-запрос
+        $.ajax({
+            url: $(this).attr('href'), // URL-адрес для загрузки всех комментариев
+            type: 'GET', // Метод запроса
+            dataType: 'html', // Тип данных, ожидаемых от сервера (html)
+            success: function(response) {
+                // Обновляем содержимое списка комментариев
+                $('#commentsList').html(response);
+            },
+            error: function() {
+                // Обработка ошибок
+                alert('Произошла ошибка при загрузке комментариев.');
+            }
+        });
+    });
+});
